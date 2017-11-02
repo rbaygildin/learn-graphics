bc8692a1ef5cee1cd8e1d2e7f974e8040cf70345
Roman Baygildin <rbaygildin95@gmail.com>
2017-11-03 00:34:41 +0300

    Init commit

---
+++ projects/polygons/polygons/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  polygons
+//
+//  Created by Max Heartfield on 03.11.17.
+//  Copyright © 2017 Roman Baygildin. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
---
+++ projects/polygons/polygons/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  polygons
+//
+//  Created by Max Heartfield on 03.11.17.
+//  Copyright © 2017 Roman Baygildin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
